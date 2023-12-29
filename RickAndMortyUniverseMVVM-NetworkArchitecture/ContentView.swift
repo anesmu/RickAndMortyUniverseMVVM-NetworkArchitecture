9c6f8aa5d6baf03b14814d4cb5ccd3064d60d119
Antonio Espino Muñoz <antonioespinom@gmail.com>
2023-12-29 16:41:02 +0100

    init project

---
+++ RickAndMortyUniverseMVVM-NetworkArchitecture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickAndMortyUniverseMVVM-NetworkArchitecture
+//
+//  Created by Antonio Espino Muñoz on 29/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickAndMortyUniverseMVVM-NetworkArchitecture/RickAndMortyUniverseMVVM_NetworkArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyUniverseMVVM_NetworkArchitectureApp.swift
+//  RickAndMortyUniverseMVVM-NetworkArchitecture
+//
+//  Created by Antonio Espino Muñoz on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyUniverseMVVM_NetworkArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
